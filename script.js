37a124d95b5effce240fa29ae4b0ecdd135cbd2b
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-05 09:17:17 +0700

    Updating padding popular

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var limit = 30; // Ganti dengan jumlah kata yang diinginkan
 
-for (var i = 1; i <= 6; i++) {
+
+for (var i = 1; i < 6; i++) {
+  var limit = 30;
   var textElement = document.getElementById("limitedText" + i);
   var text = textElement.textContent;
   var limitedText = text.split(" ").slice(0, limit).join(" ");
-  textElement.textContent = limitedText + '...';
+  textElement.textContent = limitedText + "...";
 }
+
+
+for (var i = 6; i < 10; i++) {
+    var limit = 40;
+    var textElement = document.getElementById("limitedText" + i);
+    var text = textElement.textContent;
+    var limitedText = text.split(" ").slice(0, limit).join(" ");
+    textElement.textContent = limitedText + "...";
+  }
