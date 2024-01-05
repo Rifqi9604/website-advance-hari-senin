40e0ed75a7ddac351a2f68654ea107887458c11e
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-05 09:31:52 +0700

    Update landingpage

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 for (var i = 1; i < 6; i++) {
-  var limit = 30;
+  var limit = 40;
   var textElement = document.getElementById("limitedText" + i);
   var text = textElement.textContent;
   var limitedText = text.split(" ").slice(0, limit).join(" ");
