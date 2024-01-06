b38ca0163738dddd4ca46cde8a6da2e1a9ba7011
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-06 17:42:57 +0700

    Updating landingpage

--- script.js
+++ script.js
@@ -1,65 +1,62 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if(window.innerWidth < 768){
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.innerWidth < 768) {
     for (let i = 1; i <= 6; i++) {
       let limit = 20;
       let textElement = document.getElementById("limitedText" + i);
-  
+
       if (textElement) {
         let text = textElement.textContent.trim();
         let words = text.split(" ");
         let limitedText = words.slice(0, limit).join(" ");
-  
+
         if (words.length > limit) {
           textElement.textContent = limitedText + "...";
         }
       }
     }
+  }
+});
 
-}
-}); 
+document.addEventListener("DOMContentLoaded", function () {
+  for (let i = 7; i <= 10000; i++) {
+    let limit = 25;
+    let textElement = document.getElementById("limitedText" + i);
 
-  document.addEventListener("DOMContentLoaded", function() {
-    for (let i = 7; i <= 10000; i++) {
-      let limit = 25;
-      let textElement = document.getElementById("limitedText" + i);
-  
-      if (textElement) {
-        let text = textElement.textContent.trim();
-        let words = text.split(" ");
-        let limitedText = words.slice(0, limit).join(" ");
-  
-        if (words.length > limit) {
-          textElement.textContent = limitedText + "...";
-        }
+    if (textElement) {
+      let text = textElement.textContent.trim();
+      let words = text.split(" ");
+      let limitedText = words.slice(0, limit).join(" ");
+
+      if (words.length > limit) {
+        textElement.textContent = limitedText + "...";
       }
     }
-  });
+  }
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Daftar gambar yang akan digunakan
+  let imageList = [
+    "./assets/images/icon/save-instagram.png",
+    "./assets/images/icon/save-instagram-clicked.png",
+  ];
 
-  document.addEventListener('DOMContentLoaded', function() {
-    // Daftar gambar yang akan digunakan
-    let imageList = [
-        "./assets/images/icon/save-instagram.png",
-        "./assets/images/icon/save-instagram-clicked.png"
-    ];
+  for (let i = 1; i <= 10000; i++) {
+    let saveButton = document.getElementById(`saveButton${i}`);
+    let iconImage = document.getElementById(`iconImage${i}`);
 
-    for (let i = 1; i <= 10000; i++) {
-        let saveButton = document.getElementById(`saveButton${i}`);
-        let iconImage = document.getElementById(`iconImage${i}`);
-
-        if (saveButton && iconImage) {
-            setupSaveButton(saveButton, iconImage, imageList);
-        }
+    if (saveButton && iconImage) {
+      setupSaveButton(saveButton, iconImage, imageList);
     }
+  }
 });
 
 function setupSaveButton(saveButton, iconImage, imageList) {
-    let currentIndex = 0;
+  let currentIndex = 0;
 
-    saveButton.addEventListener('click', function() {
-        iconImage.src = imageList[currentIndex];
-        currentIndex = (currentIndex + 1) % imageList.length;
-    });
+  saveButton.addEventListener("click", function () {
+    iconImage.src = imageList[currentIndex];
+    currentIndex = (currentIndex + 1) % imageList.length;
+  });
 }
 
-  
