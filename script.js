4fe31476a0cb7cab493f7ff82e1deceac45d58ba
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-06 11:01:17 +0700

    Updating bookmark icon

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    for (var i = 1; i < 10; i++) {
-      var limit = 25;
-      var textElement = document.getElementById("limitedText" + i);
+    for (let i = 1; i < 10; i++) {
+      let limit = 28;
+      let textElement = document.getElementById("limitedText" + i);
   
       if (textElement) {
-        var text = textElement.textContent.trim();
-        var words = text.split(" ");
-        var limitedText = words.slice(0, limit).join(" ");
+        let text = textElement.textContent.trim();
+        let words = text.split(" ");
+        let limitedText = words.slice(0, limit).join(" ");
   
-        // Check if there are more words to display
         if (words.length > limit) {
           textElement.textContent = limitedText + "...";
         }
       }
     }
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let saveButton = document.getElementById('saveButton');
+    let iconImage = document.getElementById('iconImage');
+
+    // Daftar gambar yang akan digunakan
+    let imageList = [
+        "./assets/images/icon/save-instagram.png",
+        "./assets/images/icon/save-instagram-clicked.png"
+    ];
+
+    let currentIndex = 0;
+
+    
+    saveButton.addEventListener('click', function() {
+        iconImage.src = imageList[currentIndex];
+        currentIndex = (currentIndex + 1) % imageList.length;
+    });
+});
+
   
