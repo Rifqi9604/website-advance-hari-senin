df0bae401f6482a7b5b9e1218590feb4bdb9f32a
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-06 09:38:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    for (var i = 1; i < 10; i++) {
-      var limit = 28;
-      var textElement = document.getElementById("limitedText" + i);
-  
-      if (textElement) {
-        var text = textElement.textContent.trim();
-        var words = text.split(" ");
-        var limitedText = words.slice(0, limit).join(" ");
-  
-        // Check if there are more words to display
-        if (words.length > limit) {
-          textElement.textContent = limitedText + "...";
+    if(window.innerWidth < 500){
+        alert("Please use a larger screen to view the website.")
+        for (var i = 1; i < 10; i++) {
+          var limit = 28;
+          var textElement = document.getElementById("limitedText" + i);
+      
+          if (textElement) {
+            var text = textElement.textContent.trim();
+            var words = text.split(" ");
+            var limitedText = words.slice(0, limit).join(" ");
+      
+            // Check if there are more words to display
+            if (words.length > limit) {
+              textElement.textContent = limitedText + "...";
+            }
+          }
         }
-      }
     }
   });
   
