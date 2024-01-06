147758e627c83d757f225a7b934404dd5285e6ca
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-06 09:39:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    if(window.innerWidth < 500){
-        alert("Please use a larger screen to view the website.")
-        for (var i = 1; i < 10; i++) {
-          var limit = 28;
-          var textElement = document.getElementById("limitedText" + i);
-      
-          if (textElement) {
-            var text = textElement.textContent.trim();
-            var words = text.split(" ");
-            var limitedText = words.slice(0, limit).join(" ");
-      
-            // Check if there are more words to display
-            if (words.length > limit) {
-              textElement.textContent = limitedText + "...";
-            }
-          }
+    for (var i = 1; i < 10; i++) {
+      var limit = 28;
+      var textElement = document.getElementById("limitedText" + i);
+  
+      if (textElement) {
+        var text = textElement.textContent.trim();
+        var words = text.split(" ");
+        var limitedText = words.slice(0, limit).join(" ");
+  
+        // Check if there are more words to display
+        if (words.length > limit) {
+          textElement.textContent = limitedText + "...";
         }
+      }
     }
   });
   
