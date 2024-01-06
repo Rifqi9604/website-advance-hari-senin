ad8827adf0e6209ecbcad24e72f3ce453ea18dc9
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-06 08:01:17 +0700

    Update trimming words

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-for (var i = 1; i < 10; i++) {
-    var limit = 60;
-    var textElement = document.getElementById("limitedText" + i);
-    var text = textElement.textContent;
-    var limitedText = text.split(" ").slice(0, limit).join(" ");
-    textElement.textContent = limitedText + "...";
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    for (var i = 1; i < 10; i++) {
+      var limit = 30;
+      var textElement = document.getElementById("limitedText" + i);
+  
+      if (textElement) {
+        var text = textElement.textContent.trim();
+        var words = text.split(" ");
+        var limitedText = words.slice(0, limit).join(" ");
+  
+        // Check if there are more words to display
+        if (words.length > limit) {
+          textElement.textContent = limitedText + "...";
+        }
+      }
+    }
+  });
+  
