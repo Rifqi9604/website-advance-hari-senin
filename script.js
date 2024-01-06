89fa42367d57ddf0beb4d2ff3480752cdec15ac2
Rifqi9604 <88605839+Rifqi9604@users.noreply.github.com>
2024-01-06 11:50:59 +0700

    Update landingpage

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-    for (let i = 1; i <= 10000; i++) {
-      let limit = 28;
+    if(window.innerWidth < 768){
+    for (let i = 1; i <= 6; i++) {
+      let limit = 20;
+      let textElement = document.getElementById("limitedText" + i);
+  
+      if (textElement) {
+        let text = textElement.textContent.trim();
+        let words = text.split(" ");
+        let limitedText = words.slice(0, limit).join(" ");
+  
+        if (words.length > limit) {
+          textElement.textContent = limitedText + "...";
+        }
+      }
+    }
+
+}
+}); 
+
+  document.addEventListener("DOMContentLoaded", function() {
+    for (let i = 7; i <= 10000; i++) {
+      let limit = 25;
       let textElement = document.getElementById("limitedText" + i);
   
       if (textElement) {
@@ -14,6 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     }
   });
+
 
   document.addEventListener('DOMContentLoaded', function() {
     // Daftar gambar yang akan digunakan
